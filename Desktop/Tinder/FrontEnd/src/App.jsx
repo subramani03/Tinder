61d9258613f94956379975116d68fea577716b3d
subramani03 <subramanimurugan420@gmail.com>
2024-12-14 13:15:02 +0530

    set up a basic structure of frontend

--- Desktop/Tinder/FrontEnd/src/App.jsx
+++ Desktop/Tinder/FrontEnd/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./index.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Body from './Components/Body';
+import Login from './Components/Login';
+import Profile from './Components/Profile';
+import Feed from './Components/Feed';
+
 const App = () => {
   return (
     <div>
-      <h1>hello </h1>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body/>}>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/feed' element={<Feed/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
